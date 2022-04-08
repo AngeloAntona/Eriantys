7d07e9f395f7bb7ea058b9dfd659792e0bc3d7e1
Luca Pirovano <contact@lucapirovano.com>
2022-04-08 10:30:44 +0200

    fix: sonarqube

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module it.polimi.ingsw.am54 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens it.polimi.ingsw.am54 to javafx.fxml;
-    exports it.polimi.ingsw.am54;
-}
